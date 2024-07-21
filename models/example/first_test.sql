2384fc1935bb86d3b538e867c2419c2dc7cb0c81
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-07-21 03:27:15 +0000

    test column tweaks

--- models/example/first_test.sql
+++ models/example/first_test.sql
@@ -1,26 +1,39 @@
-select
-    , id
-    , first_name
-    , middle_name
-    , last_name
-    , role
-    , location
-    , badge_count
-    , total_badge_count
-    , issuer_count
-    , highlighted_badge_id
-    , highlighted_badge_name
-    , highlighted_badge_date
-    , highlighted_badge_issuer
-    , most_recent_badge_id
-    , most_recent_badge_name
-    , most_recent_badge_date
-    , most_recent_badge_issuer
-    , vanity_url
-    , skills
-    , _fivetran_synced
+with
+source as (
+    select * from {{ source('scrum_community', 'credly_scrap') }}
+),
 
+final as (
 
+    select
+        -- primary key
+        _row as primary_key
 
+        , id
+        , first_name
+        , middle_name
+        , last_name
+        , role
+        , location
+        , badge_count
+        , total_badge_count
+        , issuer_count
+        , highlighted_badge_id
+        , highlighted_badge_name
+        , highlighted_badge_date
+        , highlighted_badge_issuer
+        , most_recent_badge_id
+        , most_recent_badge_name
+        , most_recent_badge_date
+        , most_recent_badge_issuer
+        , vanity_url
+        , skills
+        
+        -- metadata
+        , _fivetran_synced
 
-from {{source('scrum_community', 'credly_scrap')}}
+    from source
+
+)
+
+select * from final
