b1279588b68b713601901bc1a5949e9bdd7faf93
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-07-23 12:59:44 +0000

    comment out badge ids

--- models/staging/stg_googlesheet_credly_quebec.sql
+++ models/staging/stg_googlesheet_credly_quebec.sql
@@ -17,17 +17,17 @@ final as (
         , location
         , badge_count
         , total_badge_count
-        , issuer_count
-        , highlighted_badge_id
+        --, issuer_count
+        --, highlighted_badge_id
         , highlighted_badge_name
         , highlighted_badge_date
         , highlighted_badge_issuer
-        , most_recent_badge_id
+        --, most_recent_badge_id
         , most_recent_badge_name
         , most_recent_badge_date
         , most_recent_badge_issuer
         , vanity_url
-        , skills
+        --, skills
         
         -- metadata
         , _fivetran_synced
