4436ff9c64ece55d875a2c6bcbfd1e0b84d23ab0
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-07-30 03:07:12 +0000

    Session with CC, initial feedback

---
+++ analyses/codegen_snippets/src.sql
@@ -0,0 +1,3 @@
+{{ codegen.generate_source(schema_name= 'fivetran_google_sheets', database_name= 'scrum-community-raw', include_descriptions=true) }}
+
+-- https://hub.getdbt.com/dbt-labs/codegen/latest/
