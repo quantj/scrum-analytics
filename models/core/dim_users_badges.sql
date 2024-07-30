4d9b213b140b51c65454c508e676dd636cf5f760
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-07-30 03:44:36 +0000

    Creating dim_users_badges

---
+++ models/core/dim_users_badges.sql
@@ -0,0 +1,26 @@
+with
+directory as (
+    select * from {{ ref('stg_credly__credly_scrape') }}
+),
+users_badges as (
+    select * from {{ ref("stg_credly__credly_users_badges") }}
+),
+
+final as (
+    select
+        distinct d.id as user_id,
+        d.first_name,
+        d.last_name,
+        d.location,
+        users_badges.badge_template_name as badge_name,
+        users_badges.issued_at_date
+
+
+    from directory as d
+    left join users_badges
+        on d.vanity_url = users_badges.earner_path
+
+
+)
+
+select * from final
