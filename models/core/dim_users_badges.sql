5e644fd2713ce270f5dc4cd32e04ed110694b930
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-07-31 17:02:03 +0000

    added role + vanity url

--- models/core/dim_users_badges.sql
+++ models/core/dim_users_badges.sql
@@ -15,6 +15,8 @@ final as (
         distinct d.id as user_id,
         d.first_name,
         d.last_name,
+        d.vanity_url,
+        d.role,
         d.location,
         users_badges.badge_template_name as badge_name,
         users_badges.issued_at_date,
