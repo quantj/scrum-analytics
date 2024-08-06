0b9c324a082c33d2843a5de62ba413ef35215037
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-08-06 03:40:11 +0000

    added bunch of yml files

--- analyses/codegen_snippets/stg_yml.sql
+++ analyses/codegen_snippets/stg_yml.sql
@@ -1,6 +1,4 @@
 {{ codegen.generate_model_yaml(
-    model_names=['dim_users_badges'],
+    model_names=['stg_credly__credly_users_badges'],
     include_data_types = false
 ) }}
-
--- https://hub.getdbt.com/dbt-labs/codegen/latest/
