8997156568924ddd1e3af209e4a6593919a3eeb2
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-08-29 14:05:15 +0000

    removed old models

--- models/core/int_union_scrape.sql
+++
@@ -1,7 +0,0 @@
-with final_scrape as (
-    {{ dbt_utils.union_relations(
-    relations=[ref('stg_credly__credly_scrape'), ref('stg_credly__credly_ontario_scrape')]
-) }}
-)
-
-select * from final_scrape
