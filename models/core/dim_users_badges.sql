99761e73950d6baea06c6da8af462561804811ef
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-08-29 03:52:43 +0000

    updated dim model with new int model

--- models/core/dim_users_badges.sql
+++ models/core/dim_users_badges.sql
@@ -1,41 +1,31 @@
 with
-directory as (
-    select * from {{ ref('int_union_scrape') }}
+summary as (
+    select * from {{ ref('int_union_scrum_summary') }}
 ),
-users_badges as (
-    select 
-    *,
-    CASE
-        WHEN earn_this_badge_url IS NULL OR earn_this_badge_url = 'null' THEN 
-        url
-        ELSE 
-        earn_this_badge_url
-    END AS final_url
-    from {{ ref("int_union_users_badges") }}
+scrum_badges as (
+    select * from {{ ref("int_union_scrum_badges") }}
 ),
 
 
 final as (
     select
-        distinct d.id as user_id,
-        d.first_name,
-        CONCAT(SUBSTR(d.first_name, 1, 1), REPEAT('*', LENGTH(d.first_name) - 1)) AS masked_first_name,
-        d.last_name,
-        CONCAT(SUBSTR(d.last_name, 1, 1), REPEAT('*', LENGTH(d.last_name) - 1)) AS masked_last_name,
-        CONCAT('https://www.credly.com', d.vanity_url) as vanity_url,
-        d.role,
-        d.location,
-        users_badges.badge_template_name as badge_name,
-        users_badges.issued_at_date,
-        users_badges.issued_by,
-        users_badges.earn_this_badge_url,
-        users_badges.url,
-        users_badges.final_url as final_url
+        distinct summary.id as user_id,
+        summary.first_name,
+        CONCAT(SUBSTR(summary.first_name, 1, 1), REPEAT('*', LENGTH(summary.first_name) - 1)) AS masked_first_name,
+        summary.last_name,
+        CONCAT(SUBSTR(summary.last_name, 1, 1), REPEAT('*', LENGTH(summary.last_name) - 1)) AS masked_last_name,
+        CONCAT('https://www.credly.com', summary.vanity_url) as vanity_url,
+        summary.role,
+        summary.location,
+        scrum_badges.badge_template_name as badge_name,
+        scrum_badges.issued_at_date,
+        scrum_badges.issued_by,
+        scrum_badges.cert_url
 
 
-    from directory as d
-    left join users_badges
-        on d.vanity_url = users_badges.earner_path
+    from summary
+    left join scrum_badges
+        on summary.vanity_url = scrum_badges.earner_path
 )
 
 select * from final
