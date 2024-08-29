cbe231e6d4e4aa8b3beff9d45169ac703b019e71
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-08-29 03:46:21 +0000

    update sources to more fields, update int, moved clean up to int, and update dim_badges

--- analyses/codegen_snippets/stg_yml.sql
+++ analyses/codegen_snippets/stg_yml.sql
@@ -1,4 +1,4 @@
 {{ codegen.generate_model_yaml(
-    model_names=['stg_credly__credly_scrum_summary_quebec'],
+    model_names=['stg_credly__credly_scrum_badges_quebec'],
     include_data_types = false
 ) }}
--- models/core/dim_badges.sql
+++ models/core/dim_badges.sql
@@ -1,39 +1,15 @@
-with users_badges as (
-    select 
-    *,
-    CASE
-        WHEN earn_this_badge_url IS NULL OR earn_this_badge_url = 'null' THEN 
-        url
-        ELSE
-        earn_this_badge_url
-    END AS final_url
-    from {{ ref("int_union_users_badges") }}
-),
-
-
-final as (
+with final as (
     select
         --id,
-        name,
-        MIN(level) AS level,
+        badge_template_name AS name,
         MIN(issued_by) AS issued_by,
-        MIN(earn_this_badge_url) AS earn_this_badge_url,
-        MIN(image_url) AS image_url,
-        MIN(description) AS description,
-        MIN(cost) AS cost,
-        MIN(type_category) AS type_category,
-        MIN(global_activity_url) AS global_activity_url,
-        min(url) as URL,
-        min(final_url) as final_url,
-        min(owner_vanity_slug) as owner_vanity_slug,
-        min(vanity_slug) as vanity_slug
-        
-    from users_badges
-    group by name
-
+        MIN(cert_url) AS cert_url,
+        MIN(badge_image_url) AS badge_image_url,
+        MIN(badge_template_description) AS description,
+        MIN(cost) AS cost
+    from {{ ref('int_union_scrum_badges') }}
+    group by badge_template_name
+    order by 1
 )
-
-
-
 
 select * from final
