726f697b743ea1f15f6882a7e935f93181b9d2c4
quan.tj514@gmail.com <quan.tj514@gmail.com>
2024-12-30 17:13:13 +0000

    first version of dim gym users

--- models/core/dim_gym_users.sql
+++ models/core/dim_gym_users.sql
@@ -1,15 +1,29 @@
-with
-summary as (
-    select * from {{ ref('stg_calendar__google_events') }}
+{{ config(materialized='view') }}
+
+WITH cte AS (
+    SELECT
+        summary,
+        REGEXP_EXTRACT(summary, r'^\d+') AS id, 
+        REGEXP_REPLACE(summary, r'^\d+\s*-', '') AS name_and_service
+    FROM {{ ref('stg_calendar__google_events') }}
 ),
 
-
-final as (
-    select
-        summary
-
-
-    from summary
+cte2 AS (
+    SELECT
+        id,
+        REGEXP_EXTRACT(name_and_service, r'^[^-]*') AS name,
+        REGEXP_REPLACE(name_and_service, r'^[^-]*\s*-', '') AS service,
+        name_and_service 
+    FROM cte
 )
 
-select * from final
+SELECT
+    id,
+    name,
+    service,
+    CASE
+        WHEN name_and_service LIKE '%cancelled%' THEN 'Cancelled'
+        WHEN name_and_service LIKE '%开单看Notes%' THEN 'Open Order & Notes'
+        ELSE NULL 
+    END AS notes 
+FROM cte2
