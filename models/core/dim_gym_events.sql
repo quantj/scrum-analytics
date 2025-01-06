7f382bd3b405712928f9ce280a7fddd224b7ff8c
quan.tj514@gmail.com <quan.tj514@gmail.com>
2025-01-06 22:01:15 +0000

    adding weekday and year's week

--- models/core/dim_gym_events.sql
+++ models/core/dim_gym_events.sql
@@ -1,10 +1,12 @@
 SELECT
     id
-    , start_date_time
-    , summary
-    , description
-    , creator_email
-    , organizer_email
-    , created
-    , updated
+    ,start_date_time
+    ,FORMAT_TIMESTAMP('%A', start_date_time) AS weekday
+    ,EXTRACT(WEEK FROM start_date_time) AS week_of_year
+    ,summary
+    ,description
+    ,creator_email
+    ,organizer_email
+    ,created
+    ,updated
 FROM {{ ref('stg_calendar__google_events') }}
